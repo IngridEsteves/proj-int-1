113dfc2e67475dee9e466c6f646cfeea1d0078c2
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-05-13 19:12:25 -0300

    views-models

--- autorizacao/models.py
+++ autorizacao/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
+class Autorizacao(models.Model):
+    categoria = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+    numero = models.CharField(max_length=50)
+    emissao = models.DateField
+    validade = models.IntegerField
+    proc_ADM = models.CharField(max_length=50)
+    tcaNum = models.CharField(max_length=50)
+    compromitente = models.CharField(max_length=100)
+    cpfcnpj = models.CharField(max_length=50)
+    insc_CAD = models.CharField(max_length=5)
+    endereco = models.CharField(max_length=100)
+    quadra = models.CharField(max_length=50)
+    lote = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=100)
+    area_lote = models.IntegerField
+    sup_aut = models.IntegerField
+    matricula = models.CharField(max_length=5)
+    anuencia_CETESB = models.CharField(max_length=20)
+    anuencia_CONDEMA = models.CharField(max_length=20)
+    compensacao_averbacao = models.IntegerField
+    observacao = models.CharField(max_length=100)
+    objetivo = models.CharField(max_length=100)
+    local = models.CharField(max_length=100)
+    nativos = models.IntegerField
+    exoticos = models.IntegerField
+    euterpe = models.IntegerField
+    transplante = models.CharField(max_length=50)
+    vegetacao = models.CharField(max_length=50)
+    estagio = models.CharField(max_length=50)
+    area_aut = models.IntegerField
+    recup_PRAD = models.IntegerField
+    app1 = models.CharField(max_length=20)
+    app2 = models.CharField(max_length=20)
+    reserva = models.CharField(max_length=20)
+    restricao = models.CharField(max_length=50)
+    area_rest = models.IntegerField
+       
+    def __str__(self) -> str:
+        return self.numero
--- autorizacao/views.py
+++ autorizacao/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Autorizacao
 
 
 # Create your views here.
@@ -10,3 +11,84 @@ def autorizacao(request):
         categoria = request.POST.get('categoria')
         tipo = request.POST.get('tipo')
         numero = request.POST.get('numero')
+        emissao = request.POST.get('emissao')
+        validade = request.POST.get('validade')
+        proc_ADM = request.POST.get('proc_ADM')
+        tcaNum = request.POST.get('tcaNum')
+        compromitente = request.POST.get('compromitente')
+        cpfcnpj = request.POST.get('cpfcnpj')
+        insc_CAD = request.POST.get('insc_CAD')
+        endereco = request.POST.get('endereco')
+        quadra = request.POST.get('quadra')
+        lote = request.POST.get('lote')
+        bairro = request.POST.get('bairro')
+        area_lote = request.POST.get('area_lote')
+        sup_aut = request.POST.get('sup_aut')
+        matricula = request.POST.get('matricula')
+        anuencia_CETESB = request.POST.get('anuencia_CETESB')
+        anuencia_CONDEMA = request.POST.get('anuencia_CONDEMA')
+        compensacao_averbacao = request.POST.get('compensacao_averbacao')
+        observacao = request.POST.get('observacao')
+        objetivo = request.POST.get('objetivo')
+        local = request.POST.get('local')
+        nativos = request.POST.get('nativos')
+        exoticos = request.POST.get('exoticos')
+        euterpe = request.POST.get('euterpe')
+        transplante = request.POST.get('transplante')
+        vegetacao = request.POST.get('vegetacao')
+        estagio = request.POST.get('estagio')
+        area_aut = request.POST.get('area_aut')
+        recup_PRAD = request.POST.get('recup_PRAD')
+        app1 = request.POST.get('app1')
+        app2 = request.POST.get('app2')
+        reserva = request.POST.get('reserva')
+        restricao = request.POST.get('restricao')
+        area_rest = request.POST.get('area_rest')
+        
+        autorizacao = Autorizacao.objects.filter(numero=numero)
+        
+        if autorizacao.exists():
+            return render(request, 'autorizacao.html', {'categoria': categoria, 'tipo': tipo, 'emissao': emissao, 'validade': validade, 'proc_ADM': proc_ADM, 'tcaNum': tcaNum, 'compromitente': compromitente, 'cpfcnpj': cpfcnpj, 'insc_CAD': insc_CAD, 'endereco': endereco, 'quadra': quadra, 'lote': lote, 'bairro': bairro, 'area_lote':  area_lote, 'sup_aut': sup_aut, 'matricula': matricula, 'anuencia_CETESB': anuencia_CETESB, 'anuencia_CONDEMA': anuencia_CONDEMA, 'compensacao_averbacao': compensacao_averbacao, 'observacao': observacao, 'objetivo': objetivo, 'local': local, 'nativos': nativos, 'exoticos': exoticos, 'euterpe': euterpe, 'transplante': transplante, 'vegetacao': vegetacao, 'estagio': estagio, 'area_aut': area_aut, 'recup_PRAD': recup_PRAD, 'app1': app1, 'app2': app2, 'reserva': reserva, 'restricao': restricao, 'area_rest': area_rest})
+        
+        autorizacao = Autorizacao(
+            categoria=categoria,
+            tipo=tipo,
+            numero=numero,
+            emissao=emissao,
+            validade=validade,
+            proc_ADM=proc_ADM,
+            tcaNum=tcaNum,
+            compromitente=compromitente,
+            cpfcnpj=cpfcnpj,
+            insc_CAD=insc_CAD,
+            endereco=endereco,
+            quadra=quadra,
+            lote=lote,
+            bairro=bairro,
+            area_lote=area_lote,
+            sup_aut=sup_aut,
+            matricula=matricula,
+            anuencia_CETESB=anuencia_CETESB,
+            anuencia_CONDEMA=anuencia_CONDEMA,
+            compensacao_averbacao=compensacao_averbacao,
+            observacao=observacao,
+            objetivo=objetivo,
+            local=local,
+            nativos=nativos,
+            exoticos=exoticos,
+            euterpe=euterpe,
+            transplante=transplante,
+            vegetacao=vegetacao,
+            estagio=estagio,
+            area_aut=area_aut,
+            recup_PRAD=recup_PRAD,
+            app1=app1,
+            app2=app2,
+            reserva=reserva,
+            restricao=restricao,
+            area_rest=area_rest
+        )
+        
+        autorizacao.save()
+        
+        return HttpResponse('teste')
